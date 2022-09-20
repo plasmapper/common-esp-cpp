89e24bf9eb4ca77ea7d1b008b572063fe7848656
plasmapper <99073058+plasmapper@users.noreply.github.com>
2022-09-20 14:34:22 +0300

    Fixed TypedBuffer date creation

--- component/include/pl_typed_buffer.h
+++ component/include/pl_typed_buffer.h
@@ -16,16 +16,24 @@ public:
   Type* const data;
 
   /// @brief Create a typed buffer and allocate memory for its data
-  TypedBuffer() : Buffer (sizeof (Type)), data ((Type*)Buffer::data) {}
+  TypedBuffer() : Buffer (new Type(), sizeof (Type)), data ((Type*)Buffer::data), preallocated (false) {}
 
   /// @brief Create a typed buffer with preallocated data
   /// @param data typed buffer data pointer
-  TypedBuffer (Type* data) : Buffer (data, sizeof (Type)), data (data) {}
+  TypedBuffer (Type* data) : Buffer (data, sizeof (Type)), data (data), preallocated (true)  {}
 
   /// @brief Create a typed buffer with preallocated data and shared lockable
   /// @param data typed buffer data pointer
   /// @param lockable lockable object that is locked when this typed buffer is locked
-  TypedBuffer (Type* data, std::shared_ptr<Lockable> lockable) : Buffer (data, sizeof (Type), lockable), data (data) {}
+  TypedBuffer (Type* data, std::shared_ptr<Lockable> lockable) : Buffer (data, sizeof (Type), lockable), data (data), preallocated (true) {}
+
+  ~TypedBuffer() {
+    if (!preallocated)
+      delete data;
+  }
+
+private:
+  bool preallocated;
 };
 
 //==============================================================================
